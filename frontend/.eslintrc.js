0c8da6e760baf76799a7c5bd2e5ef285b027712b
Michele Pozzi <123.mpozzi@gmail.com>
2023-05-07 11:51:35 +0200

    Refactor: reorganize in frontend and backend folders. Add ESLint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,14 @@
+// .eslintrc.js example
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parser": '@babel/eslint-parser',
+    "parserOptions": {
+        "requireConfigFile": false,
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+  }
